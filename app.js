c58368a771c3c906c48093743a7551ec73f517da
Henry Cook <henrycook@Henrys-Air.lan>
2020-07-15 10:55:31 -0400

    Fixed bug that wouldn't show cards for certain cities

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/hfcpost/ckckvf68502cz1jmfdnzbpjki", // stylesheet location
   center: [-76.182, 42.868], // starting position [lng, lat]
-  zoom: 4.5, // starting zoom
+  zoom: 5, // starting zoom
 });
-
+const url = "https://data.ny.gov/resource/6nvr-tbv8.json";
 // Making an AXIOS get request to the New York State API.
 const apiCall = async (city) => {
-  const url = "https://data.ny.gov/resource/6nvr-tbv8.json";
+  const response = await axios.get(`${url}?program_city=${city}`);
   try {
-    const response = await axios.get(`${url}?program_city=${city}`);
     removeCards();
     elementCreator(response);
   } catch (error) {
@@ -25,8 +24,10 @@ const userInput = (e) => {
   e.preventDefault();
   const userCity = document.querySelector(".userInput").value;
   let lowerCase = userCity.toLowerCase();
-  let city = lowerCase[0].toUpperCase() + lowerCase.slice(1);
+  // let city = lowerCase[0].toUpperCase() + lowerCase.slice(1);
+  const city = lowerCase.replace(/\b\w/g, (l) => l.toUpperCase());
   document.querySelector(".userInput").value = "";
+
   apiCall(city);
 };
 // Grabbing the form and adding an event listener.
@@ -39,6 +40,7 @@ let currentMarkers = [];
 const elementCreator = (data) => {
   // Sorts the elements in data.data placing an orgainizations programs next to eachother.
   data.data.sort((a, b) => (a.agency_name > b.agency_name ? 1 : -1));
+
   // Not my code right here see => https://stackoverflow.com/questions/46155523/mapbox-clear-all-current-markers
   // needed to clear all the markers from the map when switching cities and this guy had a good solution.
   if (currentMarkers.length !== 0) {
@@ -48,15 +50,14 @@ const elementCreator = (data) => {
   }
   // Back to my code.
   for (let i = 0; i < data.data.length; i++) {
-    if (data.data[i].location.latitude) {
-      let popup = new mapboxgl.Popup({ offset: 25 })
-        .setHTML(
-          `<h1>${data.data[i].agency_name}</h1> <p>${
-            data.data[i].program_type_description
-          }</p><a href="#${[i]}">More Info</a>`
-        )
-        .setMaxWidth("300px");
-
+    let popup = new mapboxgl.Popup({ offset: 25 })
+      .setHTML(
+        `<h1>${data.data[i].agency_name}</h1> <p>${
+          data.data[i].program_type_description
+        }</p><a href="#${[i]}">More Info</a>`
+      )
+      .setMaxWidth("300px");
+    if (data.data[i].location && data.data[i].location.latitude) {
       let marker = new mapboxgl.Marker({ color: "#3e64ff" })
         .setLngLat([
           data.data[i].location.longitude,
@@ -64,6 +65,7 @@ const elementCreator = (data) => {
         ])
         .setPopup(popup)
         .addTo(map);
+      currentMarkers.push(marker);
       map.flyTo({
         center: [
           data.data[0].location.longitude,
@@ -72,10 +74,8 @@ const elementCreator = (data) => {
         zoom: 11,
         essential: true, // this animation is considered essential with respect to prefers-reduced-motion
       });
-      currentMarkers.push(marker);
     }
   }
-
   addCard(data);
 };
 
